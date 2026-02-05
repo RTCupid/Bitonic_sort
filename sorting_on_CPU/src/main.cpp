a8e5a410cab2edb965225157ebbcadea36e8a1bd
BulgakovDmitry <dima.olympiad11@yandex.ru>
2026-02-05 23:54:34 +0300

    [main.cpp] performed a trial measurement of the time taken by std::sort

--- sorting_on_CPU/src/main.cpp
+++ sorting_on_CPU/src/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <chrono>
 #include "utils.hpp"
 
 
 int main() {
-    std::vector<int> data = {5, 2, 9, 1, 5, 6};
+    using clock = std::chrono::steady_clock;
+    using duration = std::chrono::duration<double, std::milli>;
+
+    std::vector<int> data = {5, 2, 9, 1, 5, 6, 9, 3, 6, 2};
     
-    yLab::dump(data);
-    std::sort(data.begin(), data.end());
+    std::cout << "data before sorting:" << std::endl;
     yLab::dump(data);
 
+    auto start = clock::now();
+    std::sort(data.begin(), data.end());
+    auto end = clock::now();
+    duration dt = end - start;
+
+    std::cout << "data after sorting:" << std::endl;
+    yLab::dump(data);
+
+    std::cout << "Total time: " << dt.count() << " ms" << std::endl;
 }
