e5e3911c7c49ecc780628fdb89d90eb3e2017348
BulgakovDmitry <dima.olympiad11@yandex.ru>
2026-02-07 23:58:53 +0300

    [tests] create end_to_end.sh for end_toend testing with cmakes

---
+++ sorting_on_CPU/src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include "utils.hpp"
+
+int main() {
+    // using clock = std::chrono::steady_clock;
+    // using duration = std::chrono::duration<double, std::milli>;
+
+    std::size_t count;
+    std::cin >> count;
+
+    std::vector<int> data;
+    int value;
+    for (std::size_t i = 0; i < count && std::cin >> value; ++i)
+        data.push_back(value);
+
+    // std::cout << "data before sorting:" << std::endl;
+    // bLab::dump(data);
+
+    // auto start = clock::now();
+
+    std::sort(data.begin(), data.end());
+
+    // auto end = clock::now();
+    // duration dt = end - start;
+
+    // std::cout << "data after sorting:" << std::endl;
+    bLab::dump(data);
+
+    // std::cout << "Total time: " << dt.count() << " ms" << std::endl;
+}
